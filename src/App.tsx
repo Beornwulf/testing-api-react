6b1e1cc4d7ff6b1943481786baa4540f905cd9b8
Kay Sharrock <sharrock.kay@gmail.com>
2023-02-27 17:00:50 +0000

    initial API call in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
+const StarWars = () => {
+    const [person, setPerson]: [{}, ((value: (((prevState: {}) => {}) | {})) => void)] = useState({});
+
+    useEffect(() => {
+        const fetchPerson = async () => {
+            const response = await fetch("https://swapi.dev/api/people/1");
+            const data = await response.json();
+            setPerson(data);
+        };
+        fetchPerson();
+    }, []);
+    return <pre>{JSON.stringify(person, null, 2)}</pre>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <StarWars/>
+        </div>
+    );
 }
 
 export default App;
