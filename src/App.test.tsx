91b013d451aec712f5df89b8faf098abf4096d19
Kay Sharrock <sharrock.kay@gmail.com>
2023-02-28 17:04:44 +0000

    test mocking api call

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react';
+import StarWars from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test("renders the correct person from the API", async () => {
+  render(<StarWars />);
+  expect(await screen.findByText(/Luke Skywalker/)).toBeInTheDocument();
+})
